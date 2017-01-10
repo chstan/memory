ac493f9484c9cc5287dead7f45ae53283775b6aa
Conrad Stansbury <chstansbury@gmail.com>
2017-01-09 16:33:29 -0800

    Mess with webpack settings

--- webpack.local.config.js
+++ webpack.local.config.js
@@ -3,27 +3,9 @@ var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 
 var config = require('./webpack.base.config.js');
-var localSettings = require('./webpack.local-settings.js');
-
-var ip = localSettings.ip;
 
 config.devtool = "#eval-source-map";
 
-config.ip = ip;
-
-//// Use webpack dev server
-//config.entry = {
-//  app: [
-//    'webpack-dev-server/client?http://' + ip + ':3000',
-//    'webpack/hot/only-dev-server',
-//    './react/App',
-//  ],
-//};
-
-// override django's STATIC_URL for webpack bundles
-//config.output.publicPath = 'http://' + ip + ':3000' + '/assets/bundles/';
-
-// Add HotModuleReplacementPlugin and BundleTracker plugins
 config.plugins = config.plugins.concat([
   new webpack.HotModuleReplacementPlugin(),
   new webpack.NoErrorsPlugin(),
@@ -31,11 +13,9 @@ config.plugins = config.plugins.concat([
   new webpack.DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify('development'),
-      'BASE_API_URL': JSON.stringify('http://' + ip + ':8000/api/v1/'),
   }}),
 ]);
 
-// Add a loader for JSX files with react-hot enabled
 config.module.loaders.push(
   { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] }
 );
